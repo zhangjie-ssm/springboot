568744abe192d6dd0f0b3dc53e90860c058d550e
Your Name <you@example.com>
2020-09-17 14:59:14 +0800

    第一次提交spring_postgresql

---
+++ springboot+postgreSQL/src/main/java/com/jsjy/demo_01/mapper/AllPerson.java
@@ -0,0 +1,21 @@
+package com.jsjy.demo_01.mapper;
+
+import com.jsjy.demo_01.dao.person;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface AllPerson {
+    @Select("select * from test_demo")
+    List<person> getall();
+
+    @Insert("INSERT INTO test_demo (id,name,age)VALUES(#{id},#{name},#{age})")
+    int addPerson(person p);
+
+    @Update("UPDATE test_demo set name=#{name},age=#{age} WHERE id=#{id}")
+    int update(person pe);
+
+    @Delete("DELETE from test_demo WHERE id =#{id}")
+    int del(int id);
+}
---
+++ springboot+postgreSQL/src/main/java/com/jsjy/demo_01/service/AllService.java
@@ -0,0 +1,30 @@
+package com.jsjy.demo_01.service;
+
+import com.jsjy.demo_01.dao.person;
+import com.jsjy.demo_01.mapper.AllPerson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AllService {
+    @Autowired
+    private AllPerson mapper;
+
+    public List<person> getall(){
+        return mapper.getall();
+    }
+
+    public int addPerson(person p){
+        return mapper.addPerson(p);
+    }
+
+    public int update(person pe){
+        return mapper.update(pe);
+    }
+
+    public int del(int id){
+        return mapper.del(id);
+    }
+}
